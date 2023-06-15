e2e3f94402c2afc9e9630b6e88a7a2032aef673f
Jonathan Koh <jalexkoh@gmail.com>
2023-06-15 16:24:54 +0800

    add dotenv-rails

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,30 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: %i[show]
+
   def new
     @listing = Listing.new
   end
 
   def create
-    @listing = Listing.new
-    @listing.save
+    @listing = Listing.new(listing_params)
+
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def show; end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:user, :name, :address, :max_cap, :description)
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
 end
